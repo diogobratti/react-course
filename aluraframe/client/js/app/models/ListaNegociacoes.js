c5160e360b49d268959a98a5ec986f919c8d69bb
Diogo Bratti <dbratti@gmail.com>
2019-12-16 21:31:10 -0300

    Advanced JS II Lesson

--- aluraframe/client/js/app/models/ListaNegociacoes.js
+++ aluraframe/client/js/app/models/ListaNegociacoes.js
@@ -1,18 +1,26 @@
 class ListaNegociacoes {
-  constructor(){
-    this._negociacoes = [];
-  }
 
-  adiciona(negociacao){
-    this._negociacoes.push(negociacao);
-  }
+    constructor() {
+        
+        this._negociacoes = [];
+    }
 
-  esvazia() {
-    this._negociacoes = [];
-  }
+    adiciona(negociacao) {
+        
+        this._negociacoes.push(negociacao);
+    }
 
-  get negociacoes() {
-    return [...this._negociacoes];
-  }
-  
+    get negociacoes() {
+        
+        return [].concat(this._negociacoes);
+    }
+
+    esvazia() {
+        
+        this._negociacoes = [];
+    }
+    
+    get volumeTotal() {
+       return this._negociacoes.reduce((total, n) => total + n.volume, 0.0);
+    }
 }
